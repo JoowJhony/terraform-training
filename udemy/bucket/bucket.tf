80f008308a1484d499711c400d60edcf0359b326
Jonatas <jonatasrosalvo@gmail.com>
2024-11-09 23:11:47 -0300

    Colocado todos recursos na main.tf do bucket

--- udemy/bucket/bucket.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "first_bucket" {
-  bucket = "my-tf-test-bucket-joow"
-}
--- udemy/bucket/main.tf
+++ udemy/bucket/main.tf
@@ -18,3 +18,10 @@ provider "aws" {
     }
   }
 }
+
+
+### Bucket ###
+
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "my-tf-test-bucket-joow"
+}
